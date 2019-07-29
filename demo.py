3cbb80b7e9716be82393beed1acd0ad30b9c4296
R. Jordan Crouser <jcrouser@smith.edu>
2019-07-29 08:38:03 -0700

    Added demo.py

---
+++ demo.py
@@ -0,0 +1,15 @@
+#------------------------------------------------------
+#     Name(s): R. Jordan Crouser
+#        Date: 28 July 2019
+#
+# Description: This is a sample python file,
+#              including a header and a main function
+#------------------------------------------------------
+
+ def main():
+
+     name = "Jordan"
+    print("Hello, my name is", name)
+
+ if __name__ == "__main__":
+    main()
